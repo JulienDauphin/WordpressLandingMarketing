ca86d976b69e2d33e8ed3cf5fb2803a9fe1fc234
Julien Dauphin <juliendauphin.openclassrooms@gmail.com>
2023-11-18 22:11:14 +0100

    Ajout section Teamsl

--- accueil.php
+++ accueil.php
@@ -14,18 +14,26 @@
     <div class="service"><i class="fas fa-envelope"></i><h2>Marketing par E-mail</h2><p>Des campagnes personnalisées pour fidéliser vos clients et générer des leads.</p></div>
 </section>
 <section id="illustration">
-<img src="<?php echo get_template_directory_uri(); ?>/images/illustrationFull.png" alt="Illustration de nos employés pendant qu'ils travaillent" id="illustrationTop">
+<img src="<?php echo get_template_directory_uri(); ?>/images/illustrationFull.png" alt="Illustration de nos employés pendant qu'ils travaillent" id="illustrationFull">
 </section>
 <section id="methodologie">
-<div><h3>Notre Méthodologie</h3></div>
+<div><h3>Notre Méthodologie</h3>
+<span>Votre succès, Notre engagement.</span></div>
 <div id="texteMethodologie">
 <img src="<?php echo get_template_directory_uri(); ?>/images/illustrationmiddle.png" alt="Illustration de nos notre méthodologie" id="illustrationMiddle">
-
-
-
-    
-    <p>Chez MédiaPulse360, nous comprenons que chaque entreprise a sa propre histoire. Notre approche stratégique en quatre étapes commence par une analyse approfondie, suivi d'une stratégie sur mesure. Avec une mise en œuvre experte et un ajustement continu, nous vous propulsons vers le succès numérique. </p>
+<p>Chez MédiaPulse360, nous comprenons que chaque entreprise a sa propre histoire.<br>
+     Notre approche stratégique en quatre étapes commence par une analyse approfondie,<br>
+      suivi d'une stratégie sur mesure.<br> Avec une mise en œuvre experte et<br> un ajustement
+       continu, nous vous propulsons<br> vers le succès numérique. </p>
     </div>
-    
+    <div><button>Nous contacter</button></div>
+</section>
+<section id="teams">
+    <div id="theTeams">
+    <div class="team"><img src="<?php echo get_template_directory_uri(); ?>/images/portrait.png" alt="Photo portrait d'un collaborateur" ><h2>Eva</h2><p>Engagez votre public avec des stratégies sociales percutantes.</p></div>
+    <div class="team"><img src="<?php echo get_template_directory_uri(); ?>/images/portrait.png" alt="Photo portrait d'un collaborateur" ><h2>Alexandre</h2><p>Des campagnes publicitaires ciblées pour maximiser votre retour sur investissement.</p></div>
+    <div class="team"><img src="<?php echo get_template_directory_uri(); ?>/images/portrait.png" alt="Photo portrait d'un collaborateur" ><h2>Sophie</h2><p>Une approche basée sur les données pour des décisions marketing informées.</p></div>
+    <div class="team"><img src="<?php echo get_template_directory_uri(); ?>/images/portrait.png" alt="Photo portrait d'un collaborateur" ><h2>Hugo</h2><p>Des campagnes personnalisées pour fidéliser vos clients et générer des leads.</p></div>
+</div>
 </section>
 <?php get_footer() ?>
